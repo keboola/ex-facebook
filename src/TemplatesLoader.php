1ea97742e92297e69096ddface3e443ba30396d9
Ondra <ondra.jodas@gmail.com>
2023-11-22 15:38:13 +0100

    componentTemplates sync action

---
+++ src/TemplatesLoader.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\FacebookExtractor;
+
+use Exception;
+
+class TemplatesLoader
+{
+    public function __construct(readonly private string $componentId)
+    {
+    }
+
+    public function load(): array
+    {
+        $file = sprintf('%s/templates/%s.json', __DIR__, $this->componentId);
+        if (!file_exists($file)) {
+            return [];
+        }
+
+        return json_decode(
+            (string) file_get_contents($file),
+            true,
+        );
+    }
+}
