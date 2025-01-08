482c4ea9de5aa3b33216581c8f5570b2e3fed0ac
Marek Šuca <mark.suca@gmail.com>
2025-01-08 14:29:28 +0100

    let keep logger there for now

--- src/Component.php
+++ src/Component.php
@@ -112,6 +112,7 @@ class Component extends BaseComponent
     protected function getClient(): Api
     {
         $oauthData = (array) json_decode($this->getConfig()->getOAuthApiData(), true);
+        $this->getLogger()->info('OAuth data only keys: ' . json_encode(array_keys($oauthData)));
         assert(array_key_exists('token', $oauthData) || array_key_exists('access_token', $oauthData));
 
         $api = Api::init(
