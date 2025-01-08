27454b68db0423c622460d2efd704bf3a21a3139
Marek Šuca <mark.suca@gmail.com>
2025-01-08 12:04:23 +0100

    lets print out what wrong with oauth data
    lets print out what wrong with oauth data

--- src/Component.php
+++ src/Component.php
@@ -112,6 +112,7 @@ class Component extends BaseComponent
     protected function getClient(): Api
     {
         $oauthData = (array) json_decode($this->getConfig()->getOAuthApiData(), true);
+        $this->getLogger()->info(json_encode($oauthData));
         assert(array_key_exists('token', $oauthData));
 
         $api = Api::init(
